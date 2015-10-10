8c16bfbca01936804f31cff11cdab175c75e36d5
David Tang <dtang859@gmail.com>
2015-10-10 13:24:53 -0700

    more acceptance tests for the course page

--- app/controllers/courses/course.js
+++ app/controllers/courses/course.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   activeLectures: Ember.computed.filterBy('model.lectures', 'active', true),
+  activeAssignments: Ember.computed.filterBy('model.assignments', 'active', true),
   actions: {
     viewCourseDetails() {
       this.set('showDetails', true);
