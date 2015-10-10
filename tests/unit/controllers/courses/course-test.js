d08fe969c00af7b6b10e47cd9d09819d302a7e5b
David Tang <dtang859@gmail.com>
2015-10-10 13:39:11 -0700

    changed active = 1 to true. course controller unit test.

--- tests/unit/controllers/courses/course-test.js
+++ tests/unit/controllers/courses/course-test.js
@@ -1,12 +1,37 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleFor('controller:courses/course', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
+test('activeAssignments only contains assignments where active is true', function(assert) {
   var controller = this.subject();
-  assert.ok(controller);
+  var model = Ember.Object.create({
+    assignments: [
+      { active: true },
+      { active: true },
+      { active: false },
+      { active: true }
+    ]
+  });
+
+  controller.set('model', model);
+  assert.equal(controller.get('activeAssignments').length, 3);
+});
+
+test('activeLectures only contains lectures where active is true', function(assert) {
+  var controller = this.subject();
+  var model = Ember.Object.create({
+    lectures: [
+      { active: false },
+      { active: true },
+      { active: false },
+      { active: true }
+    ]
+  });
+
+  controller.set('model', model);
+  assert.equal(controller.get('activeLectures').length, 2);
 });
