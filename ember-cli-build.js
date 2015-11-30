0daf07a3377b0e92cb6064787b47d8ffa288e523
dtang <dtang@edgecast.com>
2015-11-30 09:04:45 -0800

    test build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -13,6 +13,7 @@ module.exports = function(defaults) {
   app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', { destDir: 'fonts' });
   app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', { destDir: 'fonts' });
   app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', { destDir: 'fonts' });
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
