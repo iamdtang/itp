53e15e92fba24ae93da9de71fa666682cfaf1aa3
David Tang <dtang859@gmail.com>
2016-02-09 20:08:41 -0800

    update

--- app/components/lecture-unit.js
+++ app/components/lecture-unit.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isExternal: Ember.computed('unit.link', function() {
-    var link = this.get('unit.link');
+    let link = this.get('unit.link');
 
     if (link && link.match(/https?:\/\//)) {
       return true;
